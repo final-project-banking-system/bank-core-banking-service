068b092f95e9470d7aee0af3299bbd4550d5e3e2
Marisol <marisolff121@gmail.com>
2025-12-30 12:36:49 +0300

    initial commit

---
+++ src/main/java/banking/core/BankCoreBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package banking.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCoreBankingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankCoreBankingServiceApplication.class, args);
+    }
+
+}
